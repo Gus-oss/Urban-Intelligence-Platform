b1a76d1b4c5f315f4ebe2cf1356a14632c55f77b
Gus-oss <gustavo223@live.com.mx>
2026-03-12 09:07:24 -0600

    Modifique el vite.config

--- src/phase4/vite.config.js
+++ src/phase4/vite.config.js
@@ -5,7 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    allowedHosts: 'all',
+    allowedHosts: [
+      'unneedfully-unrelaxed-sha.ngrok-free.dev',
+      '.ngrok-free.app',
+      '.ngrok.io'
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
